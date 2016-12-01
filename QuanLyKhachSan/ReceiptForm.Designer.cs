b14721d5bbb7a80e62ec13d7b9fa5c1fa5022e5f
Bui Xuan Hai <buixuanhai.it@gmail.com>
2016-12-01 19:53:04 +0700

    Change Receipt UI interface

--- QuanLyKhachSan/ReceiptForm.Designer.cs
+++ QuanLyKhachSan/ReceiptForm.Designer.cs
@@ -28,215 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.hoaDonGridView = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.khachHangTextBox = new System.Windows.Forms.TextBox();
-            this.DiaChiTextBox = new System.Windows.Forms.TextBox();
-            this.phongTextBox = new System.Windows.Forms.TextBox();
-            this.soNgayThueTextBox = new System.Windows.Forms.TextBox();
-            this.donGiaTextBox = new System.Windows.Forms.TextBox();
-            this.thanhTienTextBox = new System.Windows.Forms.TextBox();
-            this.hoaDonThanhToanGridView = new System.Windows.Forms.DataGridView();
-            this.themButton = new System.Windows.Forms.Button();
-            this.xoaButton = new System.Windows.Forms.Button();
-            this.suaButton = new System.Windows.Forms.Button();
-            this.luuButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.hoaDonThanhToanGridView)).BeginInit();
+            this.receiptIdTextBox = new System.Windows.Forms.TextBox();
+            this.searchReceiptButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.hoaDonGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // hoaDonGridView
+            // 
+            this.hoaDonGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.hoaDonGridView.Location = new System.Drawing.Point(12, 99);
+            this.hoaDonGridView.Name = "hoaDonGridView";
+            this.hoaDonGridView.Size = new System.Drawing.Size(683, 186);
+            this.hoaDonGridView.TabIndex = 12;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(244, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Khách hàng";
+            this.label1.Size = new System.Drawing.Size(176, 24);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Danh sách hóa đơn";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Location = new System.Drawing.Point(13, 60);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Địa chỉ";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Nhập mã hóa đơn";
             // 
-            // label3
+            // receiptIdTextBox
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 95);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Phòng";
+            this.receiptIdTextBox.Location = new System.Drawing.Point(113, 56);
+            this.receiptIdTextBox.Name = "receiptIdTextBox";
+            this.receiptIdTextBox.Size = new System.Drawing.Size(100, 20);
+            this.receiptIdTextBox.TabIndex = 18;
             // 
-            // label4
+            // searchReceiptButton
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 130);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Số ngày thuê";
+            this.searchReceiptButton.Location = new System.Drawing.Point(219, 55);
+            this.searchReceiptButton.Name = "searchReceiptButton";
+            this.searchReceiptButton.Size = new System.Drawing.Size(75, 23);
+            this.searchReceiptButton.TabIndex = 19;
+            this.searchReceiptButton.Text = "Tìm kiếm";
+            this.searchReceiptButton.UseVisualStyleBackColor = true;
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 162);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(44, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Đơn giá";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(13, 198);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(58, 13);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Thành tiền";
-            // 
-            // khachHangTextBox
-            // 
-            this.khachHangTextBox.Location = new System.Drawing.Point(131, 18);
-            this.khachHangTextBox.Name = "khachHangTextBox";
-            this.khachHangTextBox.Size = new System.Drawing.Size(100, 20);
-            this.khachHangTextBox.TabIndex = 6;
-            // 
-            // DiaChiTextBox
-            // 
-            this.DiaChiTextBox.Location = new System.Drawing.Point(131, 56);
-            this.DiaChiTextBox.Name = "DiaChiTextBox";
-            this.DiaChiTextBox.Size = new System.Drawing.Size(100, 20);
-            this.DiaChiTextBox.TabIndex = 7;
-            // 
-            // phongTextBox
-            // 
-            this.phongTextBox.Location = new System.Drawing.Point(131, 88);
-            this.phongTextBox.Name = "phongTextBox";
-            this.phongTextBox.Size = new System.Drawing.Size(100, 20);
-            this.phongTextBox.TabIndex = 8;
-            // 
-            // soNgayThueTextBox
-            // 
-            this.soNgayThueTextBox.Location = new System.Drawing.Point(131, 123);
-            this.soNgayThueTextBox.Name = "soNgayThueTextBox";
-            this.soNgayThueTextBox.Size = new System.Drawing.Size(100, 20);
-            this.soNgayThueTextBox.TabIndex = 9;
-            // 
-            // donGiaTextBox
-            // 
-            this.donGiaTextBox.Location = new System.Drawing.Point(131, 155);
-            this.donGiaTextBox.Name = "donGiaTextBox";
-            this.donGiaTextBox.Size = new System.Drawing.Size(100, 20);
-            this.donGiaTextBox.TabIndex = 10;
-            // 
-            // thanhTienTextBox
-            // 
-            this.thanhTienTextBox.Location = new System.Drawing.Point(131, 191);
-            this.thanhTienTextBox.Name = "thanhTienTextBox";
-            this.thanhTienTextBox.Size = new System.Drawing.Size(100, 20);
-            this.thanhTienTextBox.TabIndex = 11;
-            // 
-            // hoaDonThanhToanGridView
-            // 
-            this.hoaDonThanhToanGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.hoaDonThanhToanGridView.Location = new System.Drawing.Point(336, 18);
-            this.hoaDonThanhToanGridView.Name = "hoaDonThanhToanGridView";
-            this.hoaDonThanhToanGridView.Size = new System.Drawing.Size(324, 186);
-            this.hoaDonThanhToanGridView.TabIndex = 12;
-            // 
-            // themButton
-            // 
-            this.themButton.Location = new System.Drawing.Point(169, 230);
-            this.themButton.Name = "themButton";
-            this.themButton.Size = new System.Drawing.Size(75, 23);
-            this.themButton.TabIndex = 13;
-            this.themButton.Text = "Thêm";
-            this.themButton.UseVisualStyleBackColor = true;
-            // 
-            // xoaButton
-            // 
-            this.xoaButton.Location = new System.Drawing.Point(423, 230);
-            this.xoaButton.Name = "xoaButton";
-            this.xoaButton.Size = new System.Drawing.Size(75, 23);
-            this.xoaButton.TabIndex = 14;
-            this.xoaButton.Text = "Xóa";
-            this.xoaButton.UseVisualStyleBackColor = true;
-            // 
-            // suaButton
-            // 
-            this.suaButton.Location = new System.Drawing.Point(504, 230);
-            this.suaButton.Name = "suaButton";
-            this.suaButton.Size = new System.Drawing.Size(75, 23);
-            this.suaButton.TabIndex = 15;
-            this.suaButton.Text = "Sửa";
-            this.suaButton.UseVisualStyleBackColor = true;
-            // 
-            // luuButton
-            // 
-            this.luuButton.Location = new System.Drawing.Point(585, 230);
-            this.luuButton.Name = "luuButton";
-            this.luuButton.Size = new System.Drawing.Size(75, 23);
-            this.luuButton.TabIndex = 16;
-            this.luuButton.Text = "Lưu";
-            this.luuButton.UseVisualStyleBackColor = true;
-            // 
-            // HoaDonThanhToan
+            // ReceiptForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(707, 297);
-            this.Controls.Add(this.luuButton);
-            this.Controls.Add(this.suaButton);
-            this.Controls.Add(this.xoaButton);
-            this.Controls.Add(this.themButton);
-            this.Controls.Add(this.hoaDonThanhToanGridView);
-            this.Controls.Add(this.thanhTienTextBox);
-            this.Controls.Add(this.donGiaTextBox);
-            this.Controls.Add(this.soNgayThueTextBox);
-            this.Controls.Add(this.phongTextBox);
-            this.Controls.Add(this.DiaChiTextBox);
-            this.Controls.Add(this.khachHangTextBox);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.searchReceiptButton);
+            this.Controls.Add(this.receiptIdTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "HoaDonThanhToan";
+            this.Controls.Add(this.hoaDonGridView);
+            this.Name = "ReceiptForm";
             this.Text = "Hóa Đơn Thanh Toán";
-            ((System.ComponentModel.ISupportInitialize)(this.hoaDonThanhToanGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hoaDonGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.DataGridView hoaDonGridView;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox khachHangTextBox;
-        private System.Windows.Forms.TextBox DiaChiTextBox;
-        private System.Windows.Forms.TextBox phongTextBox;
-        private System.Windows.Forms.TextBox soNgayThueTextBox;
-        private System.Windows.Forms.TextBox donGiaTextBox;
-        private System.Windows.Forms.TextBox thanhTienTextBox;
-        private System.Windows.Forms.DataGridView hoaDonThanhToanGridView;
-        private System.Windows.Forms.Button themButton;
-        private System.Windows.Forms.Button xoaButton;
-        private System.Windows.Forms.Button suaButton;
-        private System.Windows.Forms.Button luuButton;
+        private System.Windows.Forms.TextBox receiptIdTextBox;
+        private System.Windows.Forms.Button searchReceiptButton;
     }
 }
